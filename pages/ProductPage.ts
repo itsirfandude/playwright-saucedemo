c477e573c5edf1adf8c7da7306cfce9dec665491
Irfan Sheriff <itsirfandude@Mac-mini.local>
2025-05-13 20:47:25 +0530

    Refactor: Added flows for add and remove from Cart By ProductName in ProductPage

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -8,12 +8,32 @@ export class ProductPage {
   }
 
   async getInventoryItemsCount(): Promise<number> {
-    return await this.page.locator('.inventory_item').count();
+    return await this.page.locator(".inventory_item").count();
   }
 
-async getInventoryItemTitles(): Promise<string[]> {
-  const items = this.page.locator('.inventory_item_name');
-  return await items.allTextContents();
-}
-  
+  async getInventoryItemTitles(): Promise<string[]> {
+    const items = this.page.locator(".inventory_item_name");
+    return await items.allTextContents();
+  }
+  async addToCartByProductName(productName: string) {
+    const productCard = this.page
+      .locator(".inventory_item")
+      .filter({ hasText: productName });
+    await productCard.locator("button").click();
+    return productCard;
+    // means it's in cart
+  }
+
+  async removeFromCartByProductName(productName: string) {
+    const productCard = this.page
+      .locator(".inventory_item")
+      .filter({ hasText: productName });
+    const button = productCard.locator("button");
+
+    if ((await button.textContent())?.includes("Remove")) {
+      await button.click();
+    }
+
+    return productCard;
+  }
 }
--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -2,23 +2,40 @@ import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 import { ProductPage } from "../pages/ProductPage";
 
+let productPage: ProductPage;
+
+test.beforeEach(async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.goto();
+  await loginPage.login("standard_user", "secret_sauce");
+  productPage = new ProductPage(page);
+});
 
 test("Getting product list", async ({ page }) => {
+  await expect(page).toHaveURL(/.*inventory/);
+  const count = await productPage.getInventoryItemsCount();
+  const titles = await productPage.getInventoryItemTitles();
 
-    const loginPage = new LoginPage(page);
-    await loginPage.goto();
-    await loginPage.login("standard_user", "secret_sauce");
-    await expect(page).toHaveURL(/.*inventory/);
-    const productpage = new ProductPage(page);
-    const count =  await productpage.getInventoryItemsCount();
-    const titles = await productpage.getInventoryItemTitles();
-    expect(count).toBeGreaterThan(0);
-    expect(titles.length).toBe(count);
-    console.log("Product count:", count);
-    console.log("Titles:", titles);
+  expect(count).toBeGreaterThan(0);
+  expect(titles.length).toBe(count);
+
+  console.log("Product count:", count);
+  console.log("Titles:", titles);
+});
+
+test("Managing products", async ({ page }) => {
+
+const  productCard = await productPage.addToCartByProductName("Sauce Labs Backpack");
+await expect(productCard.locator("button")).toHaveText("Remove");
+await expect(page.locator(".shopping_cart_badge")).toHaveText("1");
+console.log("Added to cart");
+await productPage.removeFromCartByProductName("Sauce Labs Backpack");
+await expect(page.locator(".shopping_cart_badge")).toHaveCount(0);
+await productPage.addToCartByProductName("Sauce Labs Onesie");
+await productPage.addToCartByProductName("Test.allTheThings() T-Shirt (Red)");
+await expect(page.locator(".shopping_cart_badge")).toHaveText("2");
+console.log("Final cart state verified");
 
 
 
-
-
-})
+});
